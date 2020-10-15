9dd2a0b78fad7d5c3e33c47d0981c900fc4ead75
helciodasilva <helciodasilva@gmail.com>
2020-10-15 05:59:51 -0300

    Aula 1: Simulando comportamentos com Mock Objects

--- teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -1,12 +1,12 @@
 package br.com.caelum.leilao.servico;
 
-import static org.junit.Assert.assertTrue;
-
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import br.com.caelum.leilao.builder.CriadorDeLeilao;
 import br.com.caelum.leilao.dominio.Leilao;
@@ -22,22 +22,17 @@ public class EncerradorDeLeilaoTest {
 
 		Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
 		Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(antiga).constroi();
-
-		LeilaoDao dao = new LeilaoDao();
-		dao.salva(leilao1);
-		dao.salva(leilao2);
+		List<Leilao> leiloesAntigos = Arrays.asList(leilao1, leilao2);
 		
-		List<Leilao> encerrados = dao.encerrados();
+		LeilaoDao daoFalso = Mockito.mock(LeilaoDao.class);
+		Mockito.when(daoFalso.correntes()).thenReturn(leiloesAntigos);
 		
-		// mas como passo os leiloes criados para o EncerradorDeLeilao,
-		// já que ele os busca no DAO?
-
-		EncerradorDeLeilao encerrador = new EncerradorDeLeilao();
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
 		encerrador.encerra();
 
-		Assert.assertEquals(2, encerrados.size());
-		assertTrue(encerrados.get(0).isEncerrado());
-		assertTrue(encerrados.get(1).isEncerrado());
+		Assert.assertEquals(2, encerrador.getTotalEncerrados());
+		Assert.assertTrue(leilao1.isEncerrado());
+		Assert.assertTrue(leilao2.isEncerrado());
 	}
 
 }
